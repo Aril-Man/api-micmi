a714b3f1d15e5e69f71350cf8e49f5abfdebc837
Azrielfhr2 <azrielfhr23@gmail.com>
2022-01-18 07:42:34 +0700

    Change method put to pacth

--- app/Http/Controllers/API/GenreController.php
+++ app/Http/Controllers/API/GenreController.php
@@ -22,16 +22,6 @@ class GenreController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -68,17 +58,6 @@ class GenreController extends Controller
             return response()->json(['Data tidak di temukan.']);
         }
         return response()->json([new GenreResource($data)]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
