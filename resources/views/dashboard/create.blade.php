8eb444a49864497224a6615c776a1fb77ab3b623
Muhamad Lutfi Azizan <mlutfiazizan@gmail.com>
2022-01-21 19:45:42 +0700

    fix dashboard view

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -19,16 +19,15 @@
     @endif
 
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Create Anime</h1>
-    </div>
+    <h1 class="h3 mb-3 text-gray-800">Create Anime</h1>
 
     <!-- Content Row -->
-    <div class="row">
         <div class="card shadow mb-4">
             <!-- Card Header - Dropdown -->
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Create Anime</h6>
+                <div class="dropdown no-arrow">
+                </div>
             </div>
             <!-- Card Body -->
             <div class="card-body">
@@ -64,6 +63,5 @@
                 </form>
             </div>
         </div>
-    </div>
 
 @endsection
